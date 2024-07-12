89e73fbba31adaea156da3c567e497448d48abc8
Michael Lindemans <michaellindemans@mw-h73chyqfnl-1.home>
2024-07-12 09:51:51 +0200

    add auto organize ts imports and relative import path

--- init.lua
+++ init.lua
@@ -389,9 +389,60 @@ lspconfig.gopls.setup({
 	handlers = handlers,
 })
 
+local function organise_imports()
+	local params = {
+		command = "_typescript.organizeImports",
+		arguments = { vim.api.nvim_buf_get_name(0) },
+		title = "",
+	}
+	vim.lsp.buf.execute_command(params)
+end
+
 lspconfig.tsserver.setup({
+	on_attach = function(client, bufnr)
+		-- Enable completion triggered by <c-x><c-o>
+		local function buf_set_option(...)
+			vim.api.nvim_buf_set_option(bufnr, ...)
+		end
+		buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+
+		-- Key mappings
+		local opts = { noremap = true, silent = true }
+		local function buf_set_keymap(...)
+			vim.api.nvim_buf_set_keymap(bufnr, ...)
+		end
+
+		-- Organize imports on save
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*.ts,*.tsx",
+			callback = function()
+				local params = {
+					command = "_typescript.organizeImports",
+					arguments = { vim.api.nvim_buf_get_name(0) },
+					title = "",
+				}
+				vim.lsp.buf.execute_command(params)
+			end,
+		})
+
+		-- Other LSP mappings and settings...
+		-- buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+		-- buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+		-- Add other key mappings as needed
+	end,
 	capabilities = capabilities,
 	handlers = handlers,
+	init_options = {
+		preferences = {
+			importModuleSpecifierPreference = "relative",
+		},
+	},
+	commands = {
+		Organise = {
+			organise_imports,
+			description = "Organise imports",
+		},
+	},
 })
 
 -- Set up lspconfig.
