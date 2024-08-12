bba370f7ca6e7fdf01f3f57194ea4da17e450ec5
Michael Lindemans <m.lindemans@bigmile.eu>
2024-08-12 22:21:55 +0200

    re-enable mouse

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -29,8 +29,6 @@ vim.opt.wildignore:append({ "*/node_modules/*" })
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.splitkeep = "cursor"
-vim.opt.mouse = ""
 
 -- Add asterisks in block comments
 vim.opt.formatoptions:append({ "r" })
-
