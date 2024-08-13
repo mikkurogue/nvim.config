eac993eaa4d348c08b369d3fae6ff0d04b1d2c14
mikku <mikkurogue@arch.user.btw>
2024-08-13 17:43:28 +0200

    update git blame to not show "not committed yuet" cause it fucking sucks

--- lua/plugins/utility.lua
+++ lua/plugins/utility.lua
@@ -49,9 +49,10 @@ return {
 			-- your configuration comes here
 			-- for example
 			enabled = true, -- if you want to enable the plugin
-			message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+			message_template = "<date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
 			date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
 			virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+			message_when_not_committed = "",
 		},
 	},
 	"hrsh7th/nvim-cmp", -- Autocompletion plugin
@@ -115,4 +116,3 @@ return {
 		},
 	},
 }
-
