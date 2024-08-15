72e7c161a76851d87437ea9dc2c8e61762084677
mikku <mikkurogue@arch.user.btw>
2024-08-15 11:22:57 +0200

    something

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,71 +1,103 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		event = "BufWritePre", -- uncomment for format on save
-		opts = require("configs.conform"),
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			require("configs.lspconfig")
-		end,
-	},
-	{
-		"BurntSushi/ripgrep",
-		lazy = false,
-	},
-	-- These are some examples, uncomment them if you want to see them work!
-	{
-		"mg979/vim-visual-multi",
-		lazy = false,
-	},
-	{
-		"folke/todo-comments.nvim",
-		lazy = false,
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {
-			-- options here...
-		},
-	},
-	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
-		"lewis6991/gitsigns.nvim",
-		lazy = false,
-		opts = {
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-			},
-		},
-	},
-	{
-		"f-person/git-blame.nvim",
-		lazy = false,
-		-- load the plugin at startup
-		event = "VeryLazy",
-		-- Because of the keys part, you will be lazy loading this plugin.
-		-- The plugin wil only load once one of the keys is used.
-		-- If you want to load the plugin at startup, add something like event = "VeryLazy",
-		-- or lazy = false. One of both options will work.
-		opts = {
-			-- your configuration comes here
-			-- for example
-			enabled = true, -- if you want to enable the plugin
-			message_template = "<date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
-			date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
-			virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
-			message_when_not_committed = "",
-		},
-	},
-	-- {
-	-- 	"nvim-treesitter/nvim-treesitter",
-	-- 	opts = {
-	-- 		ensure_installed = {
-	-- 			"vim", "lua", "vimdoc",
-	--      "html", "css"
-	-- 		},
-	-- 	},
-	-- },
-}
+    {
+      "stevearc/conform.nvim",
+      -- event = 'BufWritePre', -- uncomment for format on save
+      opts = require "configs.conform",
+    },
+    {
+      'nvimdev/dashboard-nvim',
+      event = 'VimEnter',
+      config = function()
+        require('dashboard').setup {
+          -- config
+        }
+      end,
+      dependencies = { {'nvim-tree/nvim-web-devicons'}}
+    },
+    {
+      "rebelot/heirline.nvim",  
+    },
+    { 
+        "williamboman/mason.nvim",
+        cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
+        opts = function()
+          return require "configs.mason"
+        end,
+      },
+    {
+      "neovim/nvim-lspconfig",
+      config = function()
+        require "configs.lspconfig"
+      end,
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+        opts = function()
+          return require "configs.nvimtree"
+        end,
+      },
+      {
+        "folke/which-key.nvim",
+        keys = { "<leader>", "<c-r>", "<c-w>", '"', "'", "`", "c", "v", "g" },
+        cmd = "WhichKey",
+        config = function(_, opts)
+          dofile(vim.g.base46_cache .. "whichkey")
+          require("which-key").setup(opts)
+        end,
+      },
+      {
+        "lewis6991/gitsigns.nvim",
+        event = "User FilePost",
+        opts = function()
+          return require "configs.gitsigns"
+        end,
+      },
+      {
+        "neovim/nvim-lspconfig",
+        event = "User FilePost",
+        config = function()
+          require("configs.lspconfig").defaults()
+        end,
+      },
+      {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        cmd = "Telescope",
+        opts = function()
+          return require "configs.telescope"
+        end,
+        config = function(_, opts)
+          local telescope = require "telescope"
+          telescope.setup(opts)
+    
+          -- load extensions TODO fix this
+          -- for _, ext in ipairs(opts.extensions_list) do
+          --   telescope.load_extension(ext)
+          -- end
+        end,
+      },
+      {
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPost", "BufNewFile" },
+        cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+        build = ":TSUpdate",
+        opts = function()
+          return require "configs.treesitter"
+        end,
+        config = function(_, opts)
+          require("nvim-treesitter.configs").setup(opts)
+        end,
+      },
+
+  
+    -- {
+    -- 	"nvim-treesitter/nvim-treesitter",
+    -- 	opts = {
+    -- 		ensure_installed = {
+    -- 			"vim", "lua", "vimdoc",
+    --      "html", "css"
+    -- 		},
+    -- 	},
+    -- },
+  }
