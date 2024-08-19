7bdcebd5ad19a9937efd167e5efc1b07b78c77eb
Michael Lindemans <m.lindemans@bigmile.eu>
2024-08-19 17:10:28 +0200

    something

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+-- Customize Treesitter
+
+---@type LazySpec
+return {
+	"nvim-treesitter/nvim-treesitter",
+	opts = {
+		ensure_installed = {
+			"lua",
+			"vim",
+			"typescript",
+			-- add more arguments for adding more treesitter parsers
+		},
+	},
+}
