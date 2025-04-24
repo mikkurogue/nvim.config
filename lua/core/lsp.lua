378fab41fd52f4f7af518e2181714badda75d99a
Michael Lindemans <m.lindemans@bigmile.eu>
2025-04-24 08:16:24 +0200

    remove vtsls from ensured lsps and add barbecue for breadcrumbs

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.setup()
     local lspconfig = require("lspconfig")
-    local servers = {"ts_ls", "vtsls", "rust_analyzer", "lua_ls", "pyright", "gopls", "eslint"} -- Add more as needed
+    local servers = { "ts_ls", "rust_analyzer", "lua_ls", "pyright", "gopls", "eslint" } -- Add more as needed
 
     require("mason").setup()
     require("mason-lspconfig").setup({
@@ -16,9 +16,9 @@ function M.setup()
 
     -- Configure persistent LSP diagnostics
     vim.diagnostic.config({
-        virtual_text = true, -- Show inline errors
-        signs = true, -- Show signs in the gutter
-        underline = true, -- Underline errors in code
+        virtual_text = true,    -- Show inline errors
+        signs = true,           -- Show signs in the gutter
+        underline = true,       -- Underline errors in code
         update_in_insert = true -- Keep showing errors in insert mode
     })
 end
