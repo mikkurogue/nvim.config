5b58b72fc3cb93e438995359c3ca1a9dfd2fb4e1
Michael Lindemans <m.lindemans@bigmile.eu>
2025-04-28 18:45:28 +0200

    cool stuff

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -2,24 +2,53 @@ local M = {}
 
 function M.setup()
     local lspconfig = require("lspconfig")
-    local servers = { "ts_ls", "rust_analyzer", "lua_ls", "pyright", "gopls", "eslint" } -- Add more as needed
+    local servers = {"ts_ls", "rust_analyzer", "lua_ls", "pyright", "gopls", "eslint"}
+
+    -- Get blink.cmp capabilities
+    local capabilities = require("blink.cmp").get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+    -- Common on_attach function
+    local on_attach = function(client, bufnr)
+        -- Your keybindings here
+        local opts = {
+            buffer = bufnr,
+            remap = false
+        }
+    end
 
     require("mason").setup()
     require("mason-lspconfig").setup({
-        ensure_installed = servers -- Auto-install these servers
+        ensure_installed = servers,
+        automatic_installation = true
     })
 
-    -- Setup each LSP server
+    -- Setup each LSP server with proper capabilities
     for _, server in ipairs(servers) do
-        lspconfig[server].setup({})
+        local config = {
+            capabilities = capabilities,
+            on_attach = on_attach
+        }
+
+        -- Server-specific configurations
+        if server == "lua_ls" then
+            config.settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = {"vim"}
+                    }
+                }
+            }
+        end
+
+        lspconfig[server].setup(config)
     end
 
-    -- Configure persistent LSP diagnostics
+    -- Keep your diagnostic config
     vim.diagnostic.config({
-        virtual_text = true,    -- Show inline errors
-        signs = true,           -- Show signs in the gutter
-        underline = true,       -- Underline errors in code
-        update_in_insert = true -- Keep showing errors in insert mode
+        virtual_text = true,
+        signs = true,
+        underline = true,
+        update_in_insert = true
     })
 end
 
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,8 +1,10 @@
-return {"neovim/nvim-lspconfig", -- LSP configurations
-"williamboman/mason.nvim", -- LSP installer
-"williamboman/mason-lspconfig.nvim", -- Mason integration
-{
+return {"neovim/nvim-lspconfig", "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim", {
     "jay-babu/mason-null-ls.nvim",
     event = {"BufReadPre", "BufNewFile"},
-    dependencies = {"williamboman/mason.nvim", "nvimtools/none-ls.nvim"}
+    dependencies = {"williamboman/mason.nvim", "nvimtools/none-ls.nvim"},
+    config = function()
+        require("mason-null-ls").setup({
+            automatic_installation = true
+        })
+    end
 }}
