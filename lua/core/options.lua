4288bc3eef4b9730bbc6490e68a2e88971429755
Michael Lindemans <m.lindemans@bigmile.eu>
2025-04-30 11:03:00 +0200

    some more stuffs

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,5 +1,14 @@
 vim.opt.number = true
 vim.opt.ignorecase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+
+vim.opt.wrap = false
+vim.opt.scrolloff = 16
+vim.opt.updatetime = 150
+
+vim.opt.signcolumn = "yes" 
+
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
