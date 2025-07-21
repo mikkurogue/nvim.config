9396b52fcce111dde35ba21064fefb4a39e600f1
mikkurogue <michael.lindemans@outlook.com>
2025-07-21 12:45:19 +0200

    update config

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,17 +1,24 @@
 return {
     "stevearc/conform.nvim",
-    event = {"BufWritePre"},
-    cmd = {"ConformInfo"},
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
     config = function()
         require("conform").setup({
             formatters_by_ft = {
-                javascript = {"prettier"},
-                typescript = {"prettier"},
-                javascriptreact = {"prettier"},
-                typescriptreact = {"prettier"}
+                javascript = { "biome" },
+                typescript = { "biome" },
+                javascriptreact = { "biome" },
+                typescriptreact = { "biome" }
+            },
+            formatters = {
+                biome = {
+                    command = "biome",
+                    args = { "format", "--stdin-file-path", "$FILENAME" },
+                    stdin = true,
+                },
             },
             format_on_save = {
-                pattern = {"*.js", "*.jsx", "*.ts", "*.tsx"},
+                pattern = { "*.js", "*.jsx", "*.ts", "*.tsx" },
                 timeout_ms = 10000,
                 lsp_fallback = true
             }
