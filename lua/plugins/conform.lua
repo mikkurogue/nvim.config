7c53ca7fd5a4d073acc55874a0d29002ddf642f9
mikkurogue <michael.lindemans@outlook.com>
2025-07-21 16:15:34 +0200

    add the biome_fix to the formatter so i can auto organize imports

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,10 +5,10 @@ return {
     config = function()
         require("conform").setup({
             formatters_by_ft = {
-                javascript = { "biome" },
-                typescript = { "biome" },
-                javascriptreact = { "biome" },
-                typescriptreact = { "biome" }
+                javascript = { "biome_fix", "biome" },
+                typescript = { "biome_fix", "biome" },
+                javascriptreact = { "biome_fix", "biome" },
+                typescriptreact = { "biome_fix", "biome" },
             },
             formatters = {
                 biome = {
@@ -16,11 +16,17 @@ return {
                     args = { "format", "--stdin-file-path", "$FILENAME" },
                     stdin = true,
                 },
+                biome_fix = {
+                    command = "biome",
+                    args = { "check", "--write", "$FILENAME" },
+                    stdin = false, -- biome check does not support stdin
+                    inherit = false
+                },
             },
             format_on_save = {
                 pattern = { "*.js", "*.jsx", "*.ts", "*.tsx" },
                 timeout_ms = 10000,
-                lsp_fallback = true
+                lsp_fallback = true,
             }
         })
     end
