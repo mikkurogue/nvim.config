b8a7bc84ddddea2e530b8a5bf83ddad1feea1899
mikkurogue <michael.lindemans@outlook.com>
2025-08-03 19:23:09 +0200

    use fff for file finding

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,8 +1,8 @@
-return { require("plugins.fold-imports"), require("plugins.lazygit"), require("plugins.toggleterm"), require(
+return { require("plugins.fff"), require("plugins.fold-imports"), require("plugins.lazygit"), require(
+    "plugins.toggleterm"), require(
     "plugins.lsp"), require("plugins.cmp"),
     require("plugins.conform"), require("plugins.treesitter"), require("plugins.snacks"),
     require("plugins.colorscheme"), require("plugins.lualine"), require("plugins.trouble"),
     require("plugins.snipe"), require("plugins.comment"),
     require("plugins.persistence"), require("plugins.bufferline"), require("plugins.autopairs"),
     require("plugins.crates"), require("plugins.which-key"), require("plugins.gitsigns"), require("plugins.gitui") }
--- require("plugins.telescope"),
