5ef121583078ee7b1d52f6e008d30bb23cb1734b
mikkurogue <michael.lindemans@outlook.com>
2025-08-25 14:04:12 +0300

    make the backend forced to snacks for now - wait for fff support i guesss

--- lua/plugins/action_preview.lua
+++ lua/plugins/action_preview.lua
@@ -2,5 +2,12 @@ return {
     "aznhe21/actions-preview.nvim",
     config = function()
         vim.keymap.set({ "n", "v" }, "la", require("actions-preview").code_actions)
+        require("actions-preview").setup {
+            backend = { "snacks" },
+
+            snacks = {
+                layout = { preset = "default" },
+            },
+        }
     end
 }
