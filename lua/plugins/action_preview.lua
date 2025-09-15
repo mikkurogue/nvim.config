7b6bf28cf9c62b1308ca063e2f81b7e682b1601e
mikkurogue <michael.lindemans@outlook.com>
2025-09-15 20:59:13 +0300

    update theme and stuff

--- lua/plugins/action_preview.lua
+++ lua/plugins/action_preview.lua
@@ -1,7 +1,7 @@
 return {
     "aznhe21/actions-preview.nvim",
     config = function()
-        vim.keymap.set({ "n", "v" }, "la", require("actions-preview").code_actions)
+        -- vim.keymap.set({ "n", "v" }, "la", require("actions-preview").code_actions)
         require("actions-preview").setup {
             backend = { "snacks" },
 
