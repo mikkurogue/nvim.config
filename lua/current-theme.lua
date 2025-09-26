20fae3a9b7ac89d77b684c4dc75e22a03bd13c79
mikkurogue <michael.lindemans@outlook.com>
2025-09-26 12:53:35 +0300

    install rusty theme cause its mega poggers

--- lua/current-theme.lua
+++ lua/current-theme.lua
@@ -1,1 +1,1 @@
-vim.cmd("colorscheme darkrose")
+vim.cmd("colorscheme rusty")
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,25 +1,26 @@
 return {
-{
+  { "armannikoyan/rusty" },
+  {
     "water-sucks/darkrose.nvim",
     lazy = false,
     priority = 1000,
-},
-    { "folke/tokyonight.nvim" },                                -- Multiple styles, good treesitter & LSP support
-    { "catppuccin/nvim",                 name = "catppuccin" }, -- Latte, Frappe, Macchiato, Mocha variants
-    { "EdenEast/nightfox.nvim" },                               -- Includes nightfox, dayfox, duskfox, etc.
-    { "rebelot/kanagawa.nvim" },                                -- Japanese-inspired aesthetic, warm & soft
-    { "nyoom-engineering/oxocarbon.nvim" },                     -- IBM Carbon inspired, bold and modern
-    { "rose-pine/neovim",                name = "rose-pine" },  -- Good for meditative coding sessions (already in your list)
-    { "ellisonleao/gruvbox.nvim",        priority = 1000,    config = true, },
-    {
-        "olimorris/onedarkpro.nvim",
-        name = "onedarkpro",
-        priority = 1000,
-        config = function()
-            require("onedarkpro").setup({
-                options = { style = "dark" } -- use "dark", "onelight", "vivid", etc.
-            })
-            vim.cmd("colorscheme onedark")
-        end
-    }
+  },
+  { "folke/tokyonight.nvim" }, -- Multiple styles, good treesitter & LSP support
+  { "catppuccin/nvim", name = "catppuccin" }, -- Latte, Frappe, Macchiato, Mocha variants
+  { "EdenEast/nightfox.nvim" }, -- Includes nightfox, dayfox, duskfox, etc.
+  { "rebelot/kanagawa.nvim" }, -- Japanese-inspired aesthetic, warm & soft
+  { "nyoom-engineering/oxocarbon.nvim" }, -- IBM Carbon inspired, bold and modern
+  { "rose-pine/neovim", name = "rose-pine" }, -- Good for meditative coding sessions (already in your list)
+  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
+  {
+    "olimorris/onedarkpro.nvim",
+    name = "onedarkpro",
+    priority = 1000,
+    config = function()
+      require("onedarkpro").setup {
+        options = { style = "dark" }, -- use "dark", "onelight", "vivid", etc.
+      }
+      vim.cmd "colorscheme onedark"
+    end,
+  },
 }
