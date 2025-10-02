e59faee3ded68114d3ddd72207e1c2fe8bb54a49
mikkurogue <michael.lindemans@outlook.com>
2025-10-02 20:10:12 +0300

    this now works better somehow.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,9 +1,27 @@
-return { require("plugins.fff"), require("plugins.action_preview"), require("plugins.fold-imports"), require(
-"plugins.lazygit"), require(
-    "plugins.toggleterm"), require(
-    "plugins.lsp"), require("plugins.cmp"),
-    require("plugins.conform"), require("plugins.treesitter"), require("plugins.snacks"),
-    require("plugins.colorscheme"), require("plugins.lualine"), require("plugins.trouble"),
-    require("plugins.snipe"), require("plugins.comment"),
-    require("plugins.persistence"), require("plugins.bufferline"), require("plugins.autopairs"),
-    require("plugins.crates"), require("plugins.which-key"), require("plugins.gitsigns"), require("plugins.gitui") }
+return {
+  require "plugins.ai.gh-copilot",
+  require "plugins.ai.copilot",
+  require "plugins.ai.sidekick",
+  require "plugins.fff",
+  require "plugins.action_preview",
+  require "plugins.fold-imports",
+  require "plugins.lazygit",
+  require "plugins.toggleterm",
+  require "plugins.lsp",
+  require "plugins.cmp",
+  require "plugins.conform",
+  require "plugins.treesitter",
+  require "plugins.snacks",
+  require "plugins.colorscheme",
+  require "plugins.lualine",
+  require "plugins.trouble",
+  require "plugins.snipe",
+  require "plugins.comment",
+  require "plugins.persistence",
+  require "plugins.bufferline",
+  require "plugins.autopairs",
+  require "plugins.crates",
+  require "plugins.which-key",
+  require "plugins.gitsigns",
+  require "plugins.gitui",
+}
